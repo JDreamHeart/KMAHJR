ad7dff708c9d058d53c0696474dbb814f484b894
JDreamHeart <15602291936@163.com>
2020-03-31 00:49:57 +0800

    refactor:更新分数控制逻辑

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum Direction {
     Unknown = 0,
@@ -20,11 +21,21 @@ public class GameManager : MonoBehaviour
 
     Sprite m_sprite; // 主角精灵
 
+    int m_score;
+    Text m_scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
         m_sprite = GameObject.FindGameObjectWithTag("Player").GetComponent<Sprite>();
+        // 获取游戏信息
+        Transform infos = this.transform.Find("Infos");
+        if (infos != null) {
+            m_scoreText = infos.Find("Score").GetComponent<Text>();
+        }
+        // 重置分数
+        ResetScore();
     }
 
     // Update is called once per frame
@@ -44,5 +55,23 @@ public class GameManager : MonoBehaviour
     
     public void OnStopGame() {
         m_isPlaying = false;
+    }
+
+    public void AddScore(int score) {
+        m_score += score;
+        if (m_scoreText != null) {
+            m_scoreText.text = m_score.ToString();
+        }
+    }
+    
+    public int GetScore() {
+        return m_score;
+    }
+    
+    public void ResetScore() {
+        m_score = 0;
+        if (m_scoreText != null) {
+            m_scoreText.text = m_score.ToString();
+        }
     }
 }
